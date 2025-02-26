611e6c3ed7ef5377fd5fab9813a7307790266c23
Isaac Gasi <isaac.gasi@gmail.com>
2025-02-25 23:03:21 -0800

    Added resource_id field to action list for all actions, and implement in logic.

--- lib/services/instance_manager.dart
+++ lib/services/instance_manager.dart
@@ -6,12 +6,14 @@ import '../services/instances_status.dart';
 class InstanceManagerService {
   final InstancesStatusService _statusService = InstancesStatusService();
   Future<bool> actionsHandler(
-      String url, String instanceName, List<Map<String, dynamic>> actions, Function(String) updateStatusMessage)
-  async {
-
+      String url,
+      String instanceName,
+      List<Map<String, dynamic>> actions,
+      Function(String) updateStatusMessage) async {
     final status = await _statusService.fetchStatus();
     List<dynamic> instances = status['instances'];
-    Map<String, dynamic> instance = instances.firstWhere((instance) => instance["name"].contains(instanceName));
+    Map<String, dynamic> instance = instances
+        .firstWhere((instance) => instance["name"].contains(instanceName));
 
     // print("instance:");
     // print(instance);
@@ -21,15 +23,15 @@ class InstanceManagerService {
     // print(instanceId);
     // print("------------------------------------------");
 
-
     // If instance is in the desire state the abort actions and return true.
-    if((instance["state"] == 'stopped' && actions[0]['action'] == 'turn-off')
-        || (instance["state"] == 'running' && actions[0]['action'].contains('create'))) {
+    if ((instance["state"] == 'stopped' &&
+            actions[0]['action'] == 'turn-off') ||
+        (instance["state"] == 'running' &&
+            actions[0]['action'].contains('create'))) {
       // The instance state is in the desire state.
       updateStatusMessage('${instance['name']} | Status: ${instance["state"]}');
       return true;
     }
-
 
     List<dynamic> instanceVolumes = instance['volumes'];
 
@@ -38,13 +40,16 @@ class InstanceManagerService {
     // print("------------------------------------------");
 
     // Creating a copy of the actions before modify the actions based on the instamce currrent status.
-    List<Map<String, dynamic>> filteredActions = actions.map((action) => Map<String, dynamic>.from(action)).toList();
+    List<Map<String, dynamic>> filteredActions =
+        actions.map((action) => Map<String, dynamic>.from(action)).toList();
 
     if (instance['state'] == 'stopped') {
       if (instanceVolumes.length == 2) {
         // The instance is down but the volumes are attached, only "turn-on" is required.
         //print("2 volumes attached only turn-on required ");
-        filteredActions = filteredActions.where((action) => action["action"] == "turn-on").toList();
+        filteredActions = filteredActions
+            .where((action) => action["action"] == "turn-on")
+            .toList();
       } else if (instanceVolumes.length == 1) {
         // Check if any volume contains 'root'
         // print("One volume attached, verifying if it is root or data");
@@ -53,14 +58,14 @@ class InstanceManagerService {
         // print(matchRoot ? "is root volume" : "is data volume");
         // print("Removing ${matchRoot ? 'root' : 'data'} actions");
         // Remove actions based on whether a "root" volume exists
-        filteredActions.removeWhere((item) => item['action'].contains( matchRoot ? 'root' : 'data'));
+        filteredActions.removeWhere(
+            (item) => item['action'].contains(matchRoot ? 'root' : 'data'));
       }
     }
 
     // print("actions");
     // print(filteredActions);
     // print("------------------------------------------");
-
 
     for (int i = 0; i < filteredActions.length; i++) {
       final action = filteredActions[i];
@@ -73,38 +78,44 @@ class InstanceManagerService {
           // Execute the POST request
           updateStatusMessage('Attempt to execute ${action["action"]} action');
           //print('Attempt to execute ${action["action"]} action');
-          final result = await actionResolver(url, instanceName, action['action'], null, null);
+          final result = await actionResolver(
+              url, instanceName, action['action'], null, null);
 
           if (result["succeed"]) {
             // print("succeed result:");
             // print(result);
 
+            // List of keywords to check
+            List<String> keywords = ['create', 'atach', 'snapshot'];
+
+            // Check if any of the keywords are in the action string
+            bool containsAny = keywords
+                .any((keyword) => action['action']?.contains(keyword) ?? false);
 
             // If the executed action was create-vol, it is required to get the ID fo the created vol.
-            if(action['action'].contains('create')) {
+            if (containsAny) {
               // print('${action['action']} getting volume ID');
               // print(result);
-              volId = result["responseData"]["VolumeId"];
+              volId = result["responseData"]
+                  [action["pass_condition"]['resource_id']];
               // print("Volume ID");
               // print(volId);
             }
 
-
             // verify if pass condition is required.
             if (action.containsKey("pass_condition")) {
-
               // print("Needs resolve pass_condition:");
 
-              for (int attempt = 1; attempt <= action["pass_condition"]['attempts']; attempt++) {
+              for (int attempt = 1;
+                  attempt <= action["pass_condition"]['attempts'];
+                  attempt++) {
                 String elId = volId ?? instanceId;
 
-
-
-                if(action['action'].contains('detach')) {
+                if (action['action'].contains('detach')) {
                   bool isRoot = action['action'] == "detach-root";
                   //print('Detach ${isRoot ? "root" : "data"} volume');
-                  Map volInstance = instanceVolumes.firstWhere(
-                          (volume) => volume["volume_name"].contains( isRoot ? 'root' : 'data'));
+                  Map volInstance = instanceVolumes.firstWhere((volume) =>
+                      volume["volume_name"].contains(isRoot ? 'root' : 'data'));
                   // print("volume instance to detach:");
                   // print(volInstance);
 
@@ -115,39 +126,42 @@ class InstanceManagerService {
                 //print(elId);
 
                 try {
-                  final conditionResult = await actionResolver(url, instanceName, action["pass_condition"]['action'], elId, action["pass_condition"]['response_value']);
+                  final conditionResult = await actionResolver(
+                      url,
+                      instanceName,
+                      action["pass_condition"]['action'],
+                      elId,
+                      action["pass_condition"]['response_value']);
                   // print("conditionResult:");
                   // print(conditionResult);
                   // print("------------------------------------------");
-                  if(conditionResult["succeed"]) {
+                  if (conditionResult["succeed"]) {
                     // print("succeed conditionResult:");
                     // print(conditionResult);
                     // print("breaking on success conditionResult");
                     // print("------------------------------------------");
                     success = true; // Mark success if the POST succeeds
-                    if(i == filteredActions.length - 1) {
+                    if (i == filteredActions.length - 1) {
                       //print('last action ${action["action"]} executed');
                     }
                     break actionLoop;
                   }
                 } catch (e) {
-                  updateStatusMessage('Error during attempt $attempt for action ${action["pass_condition"]["action"]}: $e');
+                  updateStatusMessage(
+                      'Error during attempt $attempt for action ${action["pass_condition"]["action"]}: $e');
                   //print('Error during attempt $attempt for action $action: $e');
                 }
 
                 // Wait for the specified time before retrying, except after the last attempt
                 if (attempt < action["pass_condition"]['attempts']) {
-                  await Future.delayed(Duration(seconds: action["pass_condition"]['timeout']));
+                  await Future.delayed(
+                      Duration(seconds: action["pass_condition"]['timeout']));
                 }
               }
-
-
-
-
             } else {
               success = true; // Mark success if the POST succeeds
               //print("breaking on success action ${action["action"]}");
-              if(i == filteredActions.length - 1) {
+              if (i == filteredActions.length - 1) {
                 //print('last action ${action["action"]} executed');
               }
               //print("------------------------------------------");
@@ -155,7 +169,8 @@ class InstanceManagerService {
             }
           }
         } catch (e) {
-          updateStatusMessage('Error during attempt $attempt for action ${action["action"]}: $e');
+          updateStatusMessage(
+              'Error during attempt $attempt for action ${action["action"]}: $e');
           //print('Error during attempt $attempt for action ${action["action"]}: $e');
         }
 
@@ -167,7 +182,8 @@ class InstanceManagerService {
 
       // Handle the case where all attempts fail
       if (!success) {
-        updateStatusMessage('Failed to execute action ${action["action"]}, stopping execution.');
+        updateStatusMessage(
+            'Failed to execute action ${action["action"]}, stopping execution.');
         //print('Failed to execute action ${action["action"]}, stopping execution.');
         return false; // Stop execution if any action fails
       }
@@ -176,19 +192,19 @@ class InstanceManagerService {
     return true;
   }
 
-  Future<Map<String, dynamic>> actionResolver(String url, String instance, String action, String? resourceId, String? conditionVal) async {
+  Future<Map<String, dynamic>> actionResolver(String url, String instance,
+      String action, String? resourceId, String? conditionVal) async {
     final uri = Uri.parse(url);
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString("token");
     final basicAuth = 'Bearer $token';
 
-    Map <String, String?>requestBody = resourceId != null ? {
-      "action": action,
-      "resource_id": resourceId
-    } : {
-      "action": action,
-      "instance": instance,
-    };
+    Map<String, String?> requestBody = resourceId != null
+        ? {"action": action, "resource_id": resourceId}
+        : {
+            "action": action,
+            "instance": instance,
+          };
 
     final response = await http.post(
       uri,
@@ -199,7 +215,7 @@ class InstanceManagerService {
       body: jsonEncode(requestBody),
     );
 
-    if(response.statusCode != 200) {
+    if (response.statusCode != 200) {
       print(response.body);
       print(response.headers);
       print(response.request);
@@ -209,9 +225,9 @@ class InstanceManagerService {
     final Map<String, dynamic> responseData = jsonDecode(response.body);
 
     return {
-      "succeed": response.statusCode == 200 && (conditionVal == null || responseData["status"] == conditionVal),
+      "succeed": response.statusCode == 200 &&
+          (conditionVal == null || responseData["status"] == conditionVal),
       "responseData": responseData
     };
-
   }
 }
